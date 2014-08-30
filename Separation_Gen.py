8f4ef886b98ec8b75619eb3ace8027560dc38e81
Qiyuan Gong <qiyuangong@gmail.com>
2014-08-30 16:53:08 +0800

    First runnable version

--- Separation_Gen.py
+++ Separation_Gen.py
@@ -1,25 +1,25 @@
 from partition_for_transaction import partition
-from mondrian import mondrian
+from mondrian_l_diversity import mondrian_l_diversity
 import time
 import pdb
 
 
 _DEBUG = True
-gl_att_tree = []
+gl_att_trees = []
 gl_data = []
 
-def Separation_Gen(att_tree, data, K=10, L=5):
+def Separation_Gen(att_trees, data, K=10, L=5):
     """Using partition_for_transaction to anonymize SA (transaction) partition, 
     while applying anatomy to separate QID and SA
     """
-    global gl_att_tree, gl_data
-    gl_att_tree = att_tree
+    global gl_att_trees, gl_data
+    gl_att_trees = att_trees
     gl_data = data
     start_time = time.time()
     print "size of dataset %d" % len(data)
     result = []
     trans = [t[-1] for t in data]
-    trans_set = partition(att_tree, trans, K)
+    trans_set = partition(att_trees[-1], trans, K)
     partition_data = []
     for ttemp in trans_set:
         (index_list, tran_value) = ttemp
@@ -28,7 +28,7 @@ def Separation_Gen(att_tree, data, K=10, L=5):
             partition_data.append(gl_data[t][:])
     print "Begin Mondrian"
     print "L=%d" % L
-    result = mondrian(partition_data, L)
+    result = mondrian_l_diversity(gl_att_trees, partition_data, L)
     print("--- %s seconds ---" % (time.time()-start_time))
     # transform data format (QID1,.., QIDn, SA set, GroupID, 1/|group size|, Group SA domain)
     # 1/|group size|, Group SA domain will be used in evaluation
--- anonymizer.py
+++ anonymizer.py
@@ -13,7 +13,7 @@ import pdb
 def get_result_one(att_trees, data, K=10, L=5):
     "fix K=10, L=5"
     data_back = copy.deepcopy(data)
-    result = Separation_Gen(att_trees[-1], data, K, L)
+    result = Separation_Gen(att_trees, data, K, L)
     data = copy.deepcopy(data_back)
     save_to_file((att_trees, data, result, K, L))
 
@@ -22,7 +22,7 @@ def get_result_K(att_trees, data, L=5):
     "change K, while fixing L"
     data_back = copy.deepcopy(data)
     for K in range(5, 55, 5):
-        result = Separation_Gen(att_trees[-1], data, K, L)
+        result = Separation_Gen(att_trees, data, K, L)
         data = copy.deepcopy(data_back)
         save_to_file((att_trees, data, result, K, L))
 
@@ -31,7 +31,7 @@ def get_result_L(att_trees, data, K=10):
     "change L, while fixing K"
     data_back = copy.deepcopy(data)
     for L in range(5, 55, 5):
-        result = Separation_Gen(att_trees[-1], data, K, L)
+        result = Separation_Gen(att_trees, data, K, L)
         data = copy.deepcopy(data_back)
         save_to_file((att_trees, data, result, K, L))
 
@@ -48,21 +48,21 @@ def get_result_dataset(att_trees, data, K=10, L=5):
         pos = i * joint
         if pos > length:
             continue
-        result = Separation_Gen(att_trees[-1], data[0:pos], K, L)
+        result = Separation_Gen(att_trees, data[0:pos], K, L)
         data = copy.deepcopy(data_back)
         save_to_file((att_trees, data[0:pos], result, K, L))
 
 
 if __name__ == '__main__':
     flag = ''
-    K = 10
-    L = 5
+    len_argv = len(sys.argv)
     try:
         flag = sys.argv[1]
-        K = int(sys.argv[2])
-        L = int(sys.argv[3])
     except:
         pass
+    #read record
+    K = 10
+    L = 5
     #read gentree tax
     att_trees = read_tree()
     #read record
@@ -74,9 +74,14 @@ if __name__ == '__main__':
         get_result_L(att_trees, data)
     elif flag == 'data':
         get_result_dataset(att_trees, data)
-    elif flag == 'Separation_Gen':
-        get_result_one(att_trees, data)
+    elif flag == 'one':
+        if len_argv > 2:
+            K = int(sys.argv[2])
+            L = int(sys.argv[3])
+            get_result_one(att_trees, data)
+        else:
+            get_result_one(att_trees, data)
     else:
-        print "Usage: python anonymizer [k | l | data |Separation_Gen]"
-    print "Finish 1M_Separation_AK!!"
+        print "Usage: python anonymizer [k | l | data | one]"
+    print "Finish 1M_Separation_Gen_KL!!"
     
